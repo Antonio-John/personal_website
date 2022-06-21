9cbc8212dd5d2c3d3fe4adcd1352c312e48c2624
Antonio-John <74070596+Antonio-John@users.noreply.github.com>
2022-06-21 20:45:20 +0100

    Update form.php

--- contact_info/form.php
+++ contact_info/form.php
@@ -4,7 +4,7 @@
     $visitor_email = $_POST['email'];
     $message = $_POST["message"];
 
-    $email_from = 'support@antoniofelton.co.uk';
+    $email_from = 'antonio.felton@virginmedia.com';
 
     $email_subject = "New Form Submission";
 
